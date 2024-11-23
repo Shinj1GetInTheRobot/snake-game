a4d6799f6fdc07ac4fc3e8f34aa24184216f3ec9
Shinji2 <zacharieetienne1@gmail.com>
2024-11-23 11:09:39 +1100

    Refactored the GameTimer to extended AnimationTimer

--- src/main/java/zen/GameTimer.java
+++ src/main/java/zen/GameTimer.java
@@ -2,36 +2,39 @@ package zen;
 
 import javafx.animation.AnimationTimer;
 
-public class GameTimer {
+public class GameTimer extends AnimationTimer {
     private final Game game;
-    private final AnimationTimer timer;
+    private final long delay;
+    private long prevNow;
     private boolean isRunning;
 
     public GameTimer(Game game, int speed) {
         this.game = game;
-        this.isRunning = false;
-        timer = new AnimationTimer() {
-            private final long delay = speed * 1000000L;
-            private long prevNow;
-            @Override
-            public void handle(long now) {
-                if (now - prevNow > delay) {
-                    game.nextFrame();
-                    prevNow = now;
-                }
-            }
-        };
+        delay = speed * 1000000L; // Convert Nanoseconds to Milliseconds
     }
 
+    @Override
+    public void handle(long now) {
+        if (now - prevNow > delay) {
+            game.nextFrame();
+            prevNow = now;
+        }
+    }
+
+    @Override
     public void start() { 
         isRunning = true;
-        timer.start();
+        super.start();
     }
+
+    @Override
     public void stop() {
-        timer.stop(); 
+        super.stop();
         isRunning = false;
     }
     public boolean isRunning() {
         return isRunning;
     }
+
+    
 }
